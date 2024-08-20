081658f4854b390f9cd1003a753a9850d29dee06
tamamo <tamamo@noemail.com>
2024-08-20 15:53:38 +0200

    update cfg

--- modules/nvchad/default.nix
+++ modules/nvchad/default.nix
@@ -8,9 +8,9 @@
   
     src = pkgs.fetchFromGitHub {
       owner = "NvChad";
-      repo = "NvChad";
-      rev = "fea5d02d2d07b148243c058d1b8b775f9cccac5e";
-      sha256 = "sha256-66pjUCD1vcrBlsVCtiNn0VVLPg2ZFNs5bfyRUh+tefA=";
+      repo = "starter";
+      rev = "3e1667ef3c5c5f74ba0783eb50984f5e2700948c";
+      sha256 = "sha256-J0iB+5aRuirhF91CnFfG8hDmkY5jOT2QlHrkoku3WVw=";
     };
   
     installPhase = ''
